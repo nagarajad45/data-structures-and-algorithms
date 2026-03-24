a7e2e6f4c3099ac2b0687c9cb916dfb09c2dc02f
Nagaraja Devaramani <nagarajadevaramani45@gmail.com>
2026-03-24 17:43:00 +0530

    Modified insertion_sort.py file and added same to Array Folder.

--- Array/insertion_sort.py
+++ Array/insertion_sort.py
@@ -6,12 +6,14 @@ def insertion_sort(numbers):
     n = len(numbers)
     for i in range(1, n):
         insert_index = i
-        curren_value = numbers.pop(i)
+        current_value = numbers.pop(i)
         for j in range(i-1, -1, -1):
-            if numbers[j] > curren_value:
+            if numbers[j] > current_value:
                 insert_index = j
-        numbers.insert(insert_index, curren_value)
+        numbers.insert(insert_index, current_value)
     return numbers
+
+
 
 numbers = [1, 4, 2, 3, 8, 9, 5, 7, 6, 10]
 result = insertion_sort(numbers)
